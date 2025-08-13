b86b61836fc6c0064c13faa8fe524946129b3000
limajonatas <jonataslimafsa@gmail.com>
2025-08-13 18:38:44 -0300

    :sparkles: feat: utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,3 +6,20 @@ export const randomColor = () => {
   }
   return color;
 };
+
+/**
+ * Receives an ISO date string and returns a string in the format "dd/MM/yyyy HH:mm"
+ * in the America/Sao_Paulo timezone.
+ * @param {string} isoDate - The ISO date string to format.
+ * @returns {string} - The formatted date string.
+ */
+export const formatDate = (isoDate) => {
+  return new Date(isoDate).toLocaleString("pt-BR", {
+    timeZone: "America/Sao_Paulo",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
