a7fe8be6848f87772ac0b21a3d76361feb9059a9
limajonatas <jonataslimafsa@gmail.com>
2025-08-13 18:38:56 -0300

    :recycle: refactor: quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -10,7 +10,7 @@
 
 
 const { configure } = require('quasar/wrappers');
-
+const path = require('path');
 
 module.exports = configure(function (/* ctx */) {
   return {
@@ -58,6 +58,9 @@ module.exports = configure(function (/* ctx */) {
       target: {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
         node: 'node16'
+      },
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
